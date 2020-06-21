5050d020da64fed81f2266c4773bf3bf193e5568
fremue85 <51433814+fremue85@users.noreply.github.com>
2020-06-21 19:13:42 +0200

    Update helper.sh

--- helper.sh
+++ helper.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-echo -n "Type something and press enter: "
-read
-echo You typed ${REPLY}
+# echo -n "Type something and press enter: "
+# read
+# echo You typed ${REPLY}
+
+cd ~
+mkdir .ssh projects
+chmod 700 .ssh
+cd .ssh
+cp /mnt/c/Users/$USER/.ssh/* .
+chmod 600 *
+chmod 644 *.pub known_hosts
+cd ../projects
+echo -e
+echo -e "\e[33mEnter your mail for Git:\e[0m"
+echo -e
+read MAIL
+echo -e
+echo -e "\e[33mEnter your username for Git:\e[0m"
+echo -e
+read USERNAME
+git config --global user.email "${MAIL}"
+git config --global user.name "${USERNAME}"
+git clone git@gitlab.com:engravo/scripts.git
+cd scripts
+git checkout feature/ubuntu-install-script-#5jhywp
